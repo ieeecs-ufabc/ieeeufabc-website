a6f0153c5baaba313a4b145ac7ab5bdad426ef9e
Nicolas-Novi <nicolas.novi09@gmail.com>
2026-09-06 11:59:16 -0300

    fix/volunteers

--- data/volunteers.ts
+++ data/volunteers.ts
@@ -2,38 +2,32 @@ export type Volunteer = {
     id: string;
     name: string;
     image: string;
-    role: string
 }
 
 export const volunteers: Volunteer[] = [
     {
-        id: "volunteer-1",
+        id: "volunteer",
         name: "Nome Sobrenome",
-        image: "/images/volunteers/volunteer1.png",
-        role: "Atuação"
+        image: "/images/volunteers/volunteer1.png"
     },
     {
         id: "volunteer",
         name: "Nome Sobrenome",
-        image: "/images/volunteers/volunteer2.png",
-        role: "Atuação"
+        image: "/images/volunteers/volunteer2.png"
     },
     {
         id: "volunteer",
         name: "Nome Sobrenome",
-        image: "/images/volunteers/volunteer3.png",
-        role: "Atuação"
+        image: "/images/volunteers/volunteer3.png"
     },
     {
         id: "volunteer",
         name: "Nome Sobrenome",
-        image: "/images/volunteers/volunteer4.png",
-        role: "Atuação"
+        image: "/images/volunteers/volunteer4.png"
     },
     {
         id: "volunteer",
         name: "Nome Sobrenome",
-        image: "/images/volunteers/volunteer5.png",
-        role: "Atuação"
-    }
+        image: "/images/volunteers/volunteer5.png"
+    },
 ]
